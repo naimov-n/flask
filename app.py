5f87e5bacd4beb7bb66d8cd59973ced322a7d3c6
oybek100 <77438276+oybek100@users.noreply.github.com>
2022-05-06 18:57:57 +0500

    new

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template, url_for
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://blog.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    content = db.Column(db.Text(500), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
